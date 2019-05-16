1c66424f1e061f80fb38a367fe26adea5635ac53
sangmai <sangmai@192.168.1.212>
2019-05-16 14:42:48 +0700

    may16

--- Exercise/src/May15/SoHoanHao.java
+++ Exercise/src/May15/SoHoanHao.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class SoHoanHao {
 	public static void main(String[] args) {
 		ArrayList<Integer> count = new ArrayList<Integer>();
-		for (int i = 2; i <= 100; i++) {
+		for (int i = 2; i <= 1000; i++) {
 			int sum = 1;
 			for (int j = 2; j <= (i/2); j++) {
 				if ((i % j) == 0) {
